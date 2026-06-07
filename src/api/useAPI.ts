e91979b311b82b34e7482970608b6f0f29229345
Andrii Serputko <aserputko@gmail.com>
2026-06-07 17:53:55 +0300

    feat: generate api

---
+++ src/api/useAPI.ts
@@ -0,0 +1,57 @@
+import { useCallback, useMemo } from 'react';
+
+import * as api from './generated';
+
+export type FetchAPI = WindowOrWorkerGlobalScope['fetch'];
+
+export function useFetchApi() {
+  return useCallback<FetchAPI>(
+    async (url, init) => {
+      const headers: HeadersInit = {};
+      const token = 'TBD Token';
+
+      if (token) {
+        headers['Authorization'] = `Bearer ${token}`;
+      } else {
+        window.console.warn('[FetchAPI][Warn]: Access token is unavailable');
+      }
+
+      const response = await fetch(url, {
+        ...init,
+        headers: {
+          ...init?.headers,
+          ...headers,
+        }
+      });
+
+      return response;
+    },
+    []
+  );
+}
+
+
+
+function useConfig() {
+  const fetchApi = useFetchApi();
+  const baseConfig = useMemo(
+    () => ({
+      basePath: 'http://localhost:4000',
+      fetchApi
+    }),
+    [fetchApi]
+  );
+
+  return useMemo(() => new api.Configuration(baseConfig), [baseConfig]);
+}
+
+const createApi =
+  <T>(ApiClass: new (config: api.Configuration) => T): (() => T) =>
+  () => {
+    const config = useConfig();
+
+    return useMemo(() => new ApiClass(config), [config]);
+  };
+
+export const useAuthApi = createApi(api.AuthApi);
+export const useSavingsApi = createApi(api.SavingsGoalsApi);
